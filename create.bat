af78e4584d60d2a6684316fb5a2322e26faa193f
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-03 17:05:09 +0200

    second commit

--- create.bat
+++ create.bat
@@ -1,10 +1,11 @@
-mkdir "C:/Users/PC_USERNAME/Desktop/CS/%2/%1"
-cd "C:/Users/PC_USERNAME/Desktop/CS/%2/%1"
+mkdir "C:/Users/Niyazov/Desktop/CS/%2/%1"
+cd "C:/Users/Niyazov/Desktop/CS/%2/%1"
 
 git init
 hub create %1
 git remote remove origin
-git remote add origin https://github.com/GITHUB_USERNAME/%1.git
+git remote add origin https://github.com/BekhruzSNiyazov/%1.git
+
 touch README.md
 git add .
 git commit -m "first commit"
